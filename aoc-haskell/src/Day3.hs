84d2ab75bcfcc9a26c5137d05d3555a3bbc1e0e5
Trevor Arjeski <tmarjeski@gmail.com>
2022-12-17 13:36:04 +0300

    Day 3...only behind by 14 days

---
+++ aoc-haskell/src/Day3.hs
@@ -0,0 +1,24 @@
+module Day3 (solve1, solve2) where
+
+import Data.Char (isLower, isUpper, ord)
+import Data.List (intersect)
+import Data.List.Split
+import Data.Set (fromList, intersection, toList)
+
+priority :: Char -> Int
+priority c
+  | isLower c = ord c - 96
+  | isUpper c = ord c - 38
+  | otherwise = error "invalid char"
+
+findDuplicate :: String -> Char
+findDuplicate s = let (left, right) = splitAt (div (length s) 2) s in last $ intersect left right
+
+findCommon :: [String] -> Char
+findCommon s = last . toList $ foldr1 intersection (fromList <$> s)
+
+solve1 :: String -> String
+solve1 input = show . sum $ priority . findDuplicate <$> lines input
+
+solve2 :: String -> String
+solve2 input = show . sum $ priority . findCommon <$> chunksOf 3 (lines input)
--- aoc-haskell/src/Lib.hs
+++ aoc-haskell/src/Lib.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
-module Lib (
-  parseArgs,
-  Error,
-  runSolution,
-) where
+module Lib
+  ( parseArgs,
+    Error,
+    runSolution,
+  )
+where
 
 import Data.Maybe
 import Day1 qualified
 import Day2 qualified
+import Day3 qualified
 import Text.Printf
 import Text.Read
 
@@ -18,14 +20,15 @@ checkDay :: String -> Either Error Int
 checkDay s
   | day <= Just 25 && day > Just 0 = Right $ fromJust day
   | otherwise = Left InvalidDay
- where
-  day = readMaybe s
+  where
+    day = readMaybe s
+
 checkPart :: String -> Either Error Int
 checkPart s
   | part == Just 1 || part == Just 2 = Right $ fromJust part
   | otherwise = Left InvalidPart
- where
-  part = readMaybe s
+  where
+    part = readMaybe s
 
 parseArgs :: [String] -> Either Error (Int, Int)
 parseArgs (d : p : _) = do
@@ -34,18 +37,20 @@ parseArgs (d : p : _) = do
   Right (day, part)
 parseArgs _ = Left ArgsError
 
-getInputFileName :: (Int, Int) -> String
-getInputFileName (day, part) = printf "./inputs/day%d_part%d.txt" day part
+getInputFileName :: Int -> String
+getInputFileName = printf "./inputs/day%d.txt"
 
 runSolution :: (Int, Int) -> IO String
 runSolution dayPart =
   do
-    let fileName = getInputFileName dayPart
+    let fileName = getInputFileName $ fst dayPart
     input <- readFile fileName
     let solver = case dayPart of
           (1, 1) -> Day1.solve1
           (1, 2) -> Day1.solve2
           (2, 1) -> Day2.solve1
           (2, 2) -> Day2.solve2
+          (3, 1) -> Day3.solve1
+          (3, 2) -> Day3.solve2
           _ -> const "no solution yet"
      in pure $ solver input
