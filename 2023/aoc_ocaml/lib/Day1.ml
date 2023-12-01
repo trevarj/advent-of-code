ff55a292f26eb803a7beddd9c5fe2754db12aa86
Trevor Arjeski <tmarjeski@gmail.com>
2023-12-01 15:37:04 +0300

    day 1 solved

--- 2023/aoc_ocaml/lib/Day1.ml
+++ 2023/aoc_ocaml/lib/Day1.ml
@@ -1,6 +1,53 @@
-let solve_part_1 input = ""
-let solve_part_2 input = ""
+open String
+
+exception DigitNotFound
+
+let is_digit = function '1' .. '9' -> true | _ -> false
+
+let rec convert_digit_words = function
+  | 'o' :: 'n' :: 'e' :: rest -> '1' :: convert_digit_words ('e' :: rest)
+  | 't' :: 'w' :: 'o' :: rest -> '2' :: convert_digit_words ('o' :: rest)
+  | 't' :: 'h' :: 'r' :: 'e' :: 'e' :: rest ->
+      '3' :: convert_digit_words ('e' :: rest)
+  | 'f' :: 'o' :: 'u' :: 'r' :: rest -> '4' :: convert_digit_words rest
+  | 'f' :: 'i' :: 'v' :: 'e' :: rest -> '5' :: convert_digit_words ('e' :: rest)
+  | 's' :: 'i' :: 'x' :: rest -> '6' :: convert_digit_words rest
+  | 's' :: 'e' :: 'v' :: 'e' :: 'n' :: rest ->
+      '7' :: convert_digit_words ('n' :: rest)
+  | 'e' :: 'i' :: 'g' :: 'h' :: 't' :: rest ->
+      '8' :: convert_digit_words ('t' :: rest)
+  | 'n' :: 'i' :: 'n' :: 'e' :: rest -> '9' :: convert_digit_words ('e' :: rest)
+  | c :: rest -> c :: convert_digit_words rest
+  | [] -> []
+
+let to_chars s = s |> to_seq |> List.of_seq
+let from_chars s = s |> List.to_seq |> of_seq
+
+let rec find_first_digit = function
+  | [] -> raise DigitNotFound
+  | c :: _ when is_digit c -> c
+  | _ :: rest -> find_first_digit rest
+
+let sum list = List.fold_left (fun acc c -> acc + c) 0 list
+
+let combine_first_last_digit s =
+  let chars = to_chars s in
+  let first = make 1 (find_first_digit chars)
+  and last = make 1 (find_first_digit (List.rev chars)) in
+  int_of_string (first ^ last)
+
+let solve_part_1 lines =
+  lines |> List.map combine_first_last_digit |> sum |> string_of_int
+
+let solve_part_2 lines =
+  lines
+  |> List.map (fun s ->
+         s |> to_chars |> convert_digit_words |> from_chars
+         |> combine_first_last_digit)
+  |> sum |> string_of_int
 
 (* tests *)
-let%test "day 1 part 1" = solve_part_1 (Util.open_data 1 1) = "[part 1 answer]"
-let%test "day 1 part 2" = solve_part_2 (Util.open_data 1 2) = "[part 2 answer]"
+let%test "day 1 part 1 sample" = Util.test_sample 1 1 solve_part_1 "142"
+let%test "day 1 part 2 sample" = Util.test_sample 1 2 solve_part_2 "281"
+let%test "day 1 part 1" = Util.test_full 1 solve_part_1 "54644"
+let%test "day 1 part 2" = Util.test_full 1 solve_part_2 "53348"
--- 2023/aoc_ocaml/lib/Util.ml
+++ 2023/aoc_ocaml/lib/Util.ml
@@ -1,3 +1,28 @@
-let open_data day part =
-  let file = Printf.sprintf "data/day%d_part%d" day part in
-  In_channel.with_open_text file (fun ic -> In_channel.input_all ic)
+let file_to_string path =
+  In_channel.with_open_text path (fun ic ->
+      String.trim @@ In_channel.input_all ic)
+
+let open_data_sample day part =
+  let file = Printf.sprintf "data/day%d_part%d_sample" day part in
+  file_to_string file
+
+let open_data day =
+  let file = Printf.sprintf "data/day%d" day in
+  file_to_string file
+
+let debug_result got want =
+  match got = want with
+  | true -> true
+  | false ->
+      print_string @@ "got: " ^ got ^ "\nexpected: " ^ want ^ "\n";
+      false
+
+let test_sample day part solution_fn want =
+  let got =
+    solution_fn (String.split_on_char '\n' @@ open_data_sample day part)
+  in
+  debug_result got want
+
+let test_full day solution_fn want =
+  let got = solution_fn (String.split_on_char '\n' @@ open_data day) in
+  debug_result got want
