1b4abf0489105f00c6dcd9c956761e0b7df08174
Trevor Arjeski <tmarjeski@gmail.com>
2023-12-01 15:38:02 +0300

    day2 prep

---
+++ 2023/aoc_ocaml/lib/Day2.ml
@@ -0,0 +1,8 @@
+let solve_part_1 lines = ""
+let solve_part_2 lines = ""
+
+(* tests *)
+let%test "day 2 part 1 sample" = Util.test_sample 2 1 solve_part_1 "[todo]"
+let%test "day 2 part 2 sample" = Util.test_sample 2 2 solve_part_2 "[todo]"
+let%test "day 2 part 1" = Util.test_full 2 solve_part_1 "[todo]"
+let%test "day 2 part 2" = Util.test_full 2 solve_part_2 "[todo]"
