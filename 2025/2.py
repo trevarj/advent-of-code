5ce593f0dbfb17b27a947008d62496d43c6fd5a9
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-02 14:16:50 +0300

    [2025] Day 2

--- 2025/2.py
+++ 2025/2.py
@@ -1,14 +1,54 @@
 from aoc import read_input
+import itertools as it
+
+
+def repeats_twice(s):
+    mid = len(s) // 2
+    return s[:mid] == s[mid:]
+
+
+def repeats_n(s):
+    chunks = (
+        list(g)
+        for k, g in it.groupby(
+            s[i : i + j + 1] for j in range(len(s)) for i in range(0, len(s), j + 1)
+        )
+    )
+    return any(
+        x
+        for x in chunks
+        if len(x) >= 2 and len(set(x)) == 1 and len(x[0]) * len(x) == len(s)
+    )
+
+
+def detect_invalid(pair, repeater):
+    start, end = map(int, pair)
+    ids = (str(i) for i in range(start, end + 1))
+    return list(map(int, filter(repeater, ids)))
+
+
+def invalid_twice(pair):
+    return detect_invalid(pair, repeats_twice)
+
+
+def invalid_n(pair):
+    return detect_invalid(pair, repeats_n)
+
+
+def input():
+    return read_input(r"(\d+)-(\d+)", ",")
+
+
+def solve(invalid):
+    return sum(sum(ids) for ids in map(invalid, input()))
 
 
 def solve1():
-    data = read_input()
-    pass
+    return solve(invalid_twice)
 
 
 def solve2():
-    data = read_input()
-    pass
+    return solve(invalid_n)
 
 
 print(solve1())
--- 2025/aoc.py
+++ 2025/aoc.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+import re
 from pathlib import Path
 
 
@@ -8,20 +9,28 @@ def int_parser(lines):
     return [list(map(int, line.split())) for line in lines if line]
 
 
-def read_input(parser=None):
+def read_input(parser=None, splitter="\n", input=None):
     """
     Automatically read the AoC input corresponding to the calling file.
-    """
-    filename = inspect.stack()[1].filename
-    caller_path = Path(filename).resolve()
-    day = caller_path.stem
 
-    input_path = Path("inputs") / f"{day}.txt"
-    lines = input_path.read_text().rstrip("\n").splitlines()
-    if callable(parser):
-        return parser(lines)
+    Parser can be a function that takes a list of strings (lines), or a regex
+    string which will be matched upon and a tuple of the groups is returned.
+    """
+    if input is None:
+        filename = inspect.stack()[1].filename
+        caller_path = Path(filename).resolve()
+        day = caller_path.stem
+
+        input_path = Path("inputs") / f"{day}.txt"
+        splits = input_path.read_text().rstrip("\n").split(splitter)
     else:
-        return lines
+        splits = input.rstrip("\n").split(splitter)
+    if callable(parser):
+        return parser(splits)
+    elif isinstance(parser, str):
+        return [re.match(parser, line).groups() for line in splits]
+    else:
+        return splits
 
 
 def generate_days(days=12):
