31c41a86107dad372fdc15e65b457de736c544d6
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-04 23:36:28 +0300

    [2025] Day 4 small improvement

--- 2025/4.py
+++ 2025/4.py
@@ -1,5 +1,6 @@
 from aoc import read_input
 from collections import defaultdict
+import time
 
 
 def adjacent(pos, nodes):
@@ -24,27 +25,36 @@ def nodes(grid):
     return d
 
 
+def is_free(pos, ns, stage):
+    return (
+        sum(1 for n in adjacent(pos, ns) if n == "@" or (stage == 1 and n == "x")) < 4
+    )
+
+
 def solve():
     grid = read_input(mapper=list)
     ns = nodes(grid)
     accessible = None
     count = 0
-    removeable = set([1])
-    while removeable:
-        removeable = set()
+    removed = -1
+    stage = 1
+    while removed != 0:
+        removed = 0
         for k, v in list(ns.items()):
-            if v == "@" and adjacent(k, ns).count("@") < 4:
-                removeable.add(k)
+            if v == "@" and is_free(k, ns, stage):
+                ns[k] = "x"
                 count += 1
+                removed += 1
+        stage += 1
         if accessible is None:
-            accessible = count
-        ns = defaultdict(
-            lambda: None, {k: v for k, v in ns.items() if k not in removeable}
-        )
+            accessible = removed
 
     return accessible, count
 
 
+t = time.time()
 p1, p2 = solve()
+end = time.time()
 print(p1)
 print(p2)
+print(f"elapsed time {end - t}")
