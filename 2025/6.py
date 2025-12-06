d43eb0908a5ac0ae3e6097bac3e621058d7dfd7b
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-06 19:41:06 +0300

    [2025] cleanup day 6

--- 2025/6.py
+++ 2025/6.py
@@ -5,33 +5,29 @@ import operator
 ops = {"+": operator.add, "*": operator.mul}
 
 
-def solve(args):
-    return reduce(ops[args[-1]], map(int, args[:-1]))
+def solve(acc, args):
+    return acc + reduce(ops[args[-1]], map(int, args[:-1]))
 
 
 def solve1():
     data = read_input(splitter=["\n", " "])
-    return sum(solve([n[col] for n in data]) for col in range(len(data[0])))
+    return reduce(solve, ([n[col] for n in data] for col in range(len(data[0]))), 0)
 
 
 def solve2():
     data = read_input()
-    reversed = [[ch for ch in line[::-1]] for line in data]
-    op_list = [op for op in reversed[-1] if op != " "]
-    aligned = ([n[col] for n in reversed[:-1]] for col in range(len(reversed[0])))
+    op_list = [op for op in data[-1] if op != " "]
+    aligned = (([n[col] for n in data[:-1]]) for col in range(len(data[0])))
     problems = []
     curr = []
     for p in aligned:
         if all(n == " " for n in p):
-            problems.append(curr)
+            problems.append(curr + [op_list.pop(0)])
             curr = []
             continue
-        curr.append(int("".join(p)))
-    problems.append(curr)
-    total = 0
-    for op, nums in zip(op_list, problems):
-        total += reduce(ops[op], nums)
-    return total
+        curr.append("".join(p))
+    problems.append(curr + [op_list.pop(0)])
+    return reduce(solve, problems, 0)
 
 
 print(solve1())
