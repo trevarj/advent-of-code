09786af9a2ebf536fea903ca3dd6b5155752815f
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-06 16:15:31 +0300

    [2025] Day 6

--- 2025/6.py
+++ 2025/6.py
@@ -1,14 +1,37 @@
 from aoc import read_input
+from functools import reduce
+import operator
+
+ops = {"+": operator.add, "*": operator.mul}
+
+
+def solve(args):
+    return reduce(ops[args[-1]], map(int, args[:-1]))
 
 
 def solve1():
-    data = read_input()
-    pass
+    data = read_input(splitter=["\n", " "])
+    return sum(solve([n[col] for n in data]) for col in range(len(data[0])))
 
 
 def solve2():
     data = read_input()
-    pass
+    reversed = [[ch for ch in line[::-1]] for line in data]
+    op_list = [op for op in reversed[-1] if op != " "]
+    aligned = ([n[col] for n in reversed[:-1]] for col in range(len(reversed[0])))
+    problems = []
+    curr = []
+    for p in aligned:
+        if all(n == " " for n in p):
+            problems.append(curr)
+            curr = []
+            continue
+        curr.append(int("".join(p)))
+    problems.append(curr)
+    total = 0
+    for op, nums in zip(op_list, problems):
+        total += reduce(ops[op], nums)
+    return total
 
 
 print(solve1())
