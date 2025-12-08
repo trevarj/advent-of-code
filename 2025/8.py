6137a1b8776be56c39bb44df27ca269fbac93161
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-08 21:27:10 +0300

    [2025] Day 8

--- 2025/8.py
+++ 2025/8.py
@@ -1,15 +1,34 @@
 from aoc import read_input
+from functools import reduce
+import math
+import itertools as it
+import operator
 
 
-def solve1():
-    data = read_input()
-    pass
+def solve():
+    data = read_input(
+        splitter=["\n", ","],
+        mapper=lambda x: tuple(map(int, x)),
+    )
+    connections = sorted(it.combinations(data, 2), key=lambda x: math.dist(*x))
+    circuits = {node: {node} for pair in connections for node in pair}
+    for i, (a, b) in enumerate(connections):
+        for c in circuits:
+            if a in circuits[c]:
+                c1 = c
+            if b in circuits[c]:
+                c2 = c
+        if c1 != c2:
+            circuits[c1] |= circuits[c2]
+            del circuits[c2]
+        if i + 1 == 1000:
+            yield reduce(
+                operator.mul, sorted(map(len, [circuits[b] for b in circuits]))[-3:]
+            )
+        if len(circuits) == 1:
+            yield a[0] * b[0]
 
 
-def solve2():
-    data = read_input()
-    pass
-
-
-print(solve1())
-print(solve2())
+g = solve()
+print(next(g))
+print(next(g))
