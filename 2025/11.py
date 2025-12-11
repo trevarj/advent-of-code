7bb8671d7a2c0d0148f0410b9091e6b4492b352f
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-11 16:01:11 +0300

    [2025] Day 11

--- 2025/11.py
+++ 2025/11.py
@@ -1,15 +1,22 @@
 from aoc import read_input
+from functools import cache
 
 
-def solve1():
-    data = read_input()
-    pass
+def solve():
+    data = {k[:-1]: v for k, *v in read_input(mapper=str.split)}
+
+    @cache
+    def flow(node, fft, dac):
+        match node:
+            case "out":
+                return fft and dac
+            case "fft":
+                fft = True
+            case "dac":
+                dac = True
+        return sum(flow(next, fft, dac) for next in data[node])
+
+    return flow("you", True, True), flow("svr", False, False)
 
 
-def solve2():
-    data = read_input()
-    pass
-
-
-print(solve1())
-print(solve2())
+print(solve())
