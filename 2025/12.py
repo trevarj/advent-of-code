faf82a22666ae61065e6e356aa05a0939639a603
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-12 10:50:18 +0300

    [2025] Day 12

--- 2025/12.py
+++ 2025/12.py
@@ -1,15 +1,18 @@
 from aoc import read_input
 
 
-def solve1():
-    data = read_input()
-    pass
+def solve():
+    data = read_input(splitter=["\n\n"])
+    print(data)
+    regions = [
+        (tuple(map(int, k.split("x"))), list(map(int, v.split())))
+        for k, v in (r.split(": ") for r in data[-1].split("\n"))
+    ]
+    count = 0
+    for region, values in regions:
+        x, y = region
+        count += x // 3 * y // 3 >= sum(values)
+    return count
 
 
-def solve2():
-    data = read_input()
-    pass
-
-
-print(solve1())
-print(solve2())
+print(solve())
